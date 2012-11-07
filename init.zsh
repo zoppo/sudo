119ab004ec6e4523c0a4394100c60a96ff079053
meh <meh@schizofreni.co>
2012-11-07 13:26:30 +0100

    Refactor everything

--- init.zsh
+++ init.zsh
@@ -5,12 +5,34 @@ if ! is-callable sudo; then
   }
 fi
 
-zdefault -a ':zoppo:plugin:aliases' environments envs \
-  'arch' 'utils'
+if zdefault -t ':zoppo:plugin:sudo' all 'no'; then
+  for program (dispatchers/*(:t))
+    source-relative "dispatchers/$program"
+  unset program
+else
+  zdefault ':zoppo:plugin:sudo:environment' arch \
+    'pacman' 'pacman-color' 'yaourt' 'packer' 'abs'
 
-for env ("$envs[@]")
-  source-relative "dispatchers/$env" || warn "dispatchers: $env: environment not found"
+  zdefault ':zoppo:plugin:sudo:environment' utils \
+    'mount' 'umount' 'shutdown' 'reboot' 'poweroff' 'halt' 'pm-suspend'
 
-unset envs env
+  zdefault -a ':zoppo:plugin:sudo' programs programs \
+    'pacman' 'pacman-color'
+
+  for program ("$programs[@]")
+    source-relative "dispatchers/$program" || warn "sudo: $program: program not found"
+  unset program programs
+
+  zstyle -a ':zoppo:plugin:sudo' environments environments
+
+  for environment ($environments[@]); do
+    zstyle -a ':zoppo:plugin:sudo:environment' "$environment" programs
+
+    for program ("$programs[@]")
+      source-relative "dispatchers/$program" || warn "sudo: $program: program not found"
+    unset program programs
+  done
+  unset environment environments
+fi
 
 # vim: ft=zsh sts=2 ts=2 sw=2 et fdm=marker fmr={{{,}}}
