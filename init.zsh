510e4b62be6caca5aeeadcdbd12653090b3b1abb
meh <meh@schizofreni.co>
2012-11-09 23:33:48 +0100

    Add a gazillion dispatchers

--- init.zsh
+++ init.zsh
@@ -5,17 +5,25 @@ if ! is-callable sudo; then
   }
 fi
 
+# Environments {{{
+zdefault ':zoppo:plugin:sudo:environment' utils \
+  'cat' 'tail' 'rm' 'mv' 'cp' 'chown' 'chmod' 'chgrp' 'mkdir'
+
+zdefault ':zoppo:plugin:sudo:environment' system \
+  'mount' 'umount' 'shutdown' 'reboot' 'poweroff' 'halt' 'pm-suspend'
+
+zdefault ':zoppo:plugin:sudo:environment' arch \
+  'pacman' 'pacman-color' 'yaourt' 'packer' 'abs'
+
+zdefault ':zoppo:plugin:sudo:environment' gentoo \
+  'emerge' 'eselect' 'revdep-rebuild' 'perl-cleaner' 'lafilefixer' 'python-updater' 'layman'
+# }}}
+
 if zdefault -t ':zoppo:plugin:sudo' all 'no'; then
   for program (dispatchers/*(:t))
     source-relative "dispatchers/$program"
   unset program
 else
-  zdefault ':zoppo:plugin:sudo:environment' arch \
-    'pacman' 'pacman-color' 'yaourt' 'packer' 'abs'
-
-  zdefault ':zoppo:plugin:sudo:environment' utils \
-    'mount' 'umount' 'shutdown' 'reboot' 'poweroff' 'halt' 'pm-suspend'
-
   zdefault -a ':zoppo:plugin:sudo' programs programs \
     'pacman' 'pacman-color'
 
